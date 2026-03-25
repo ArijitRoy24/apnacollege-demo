cc51be5db8fbd7b6c777a0eb82ec79158e6c66d1
ArijitRoy24 <atroy2019@outlook.com>
2026-03-26 01:35:33 +0600

    Add new paragraph

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # apnacollege-demo
 This is my first Git repository
 <br>
-Author - Arijit Roy
+Author - Arijit Roy (SUST)
---
+++ index.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define int long long
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+void solve(int tc){
+    cout << "hello, world!\n";
+}
+int32_t main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1, tc = 1;
+    // cin >> t;
+    // cout << fixed << setprecision(10);
+    while(t--) {
+        solve(tc++);
+    }
+    return 0;
+}
